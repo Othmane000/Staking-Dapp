0b7ca742d0df2ed0450e1bac3e08bc181120c840
Othmane000 <75754374+Othmane000@users.noreply.github.com>
2022-06-14 14:24:37 +0100

    smart-contracts (still working on them)

--- smart-contracts/NFT721.sol
+++ smart-contracts/NFT721.sol
@@ -6,31 +6,68 @@ pragma solidity ^0.8.14 ;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol" ;
 import "@openzeppelin/contracts/utils/Strings.sol";
-//import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract NFT721 is ERC721 {
+contract NFT721 is ERC721, Ownable {
 
     uint256 public totalsupply = 0 ;
-    uint256 maxTokensToMint;
-    uint256 Supply ;
+    uint256 public maxSupply ;
+    uint256 public price = 0.01 ether ; // 0.01 ether -> 30000000000000000 wei 
+    
     mapping(uint256 => bool) public NFTstock ; // this will check if a token is available or not
-    mapping(uint => address) private NFTOwner ;  // this will take care of associating a nft with its owner.
-    constructor() ERC721("Othmane","TOTO"){
-        Supply = 200 ;
-        maxTokensToMint = 10 ;
+    mapping(address => bool) public approval ;
+
+    event NFTminted(
+        uint256 indexed tokenid,
+        uint256 indexed timestamp,
+        address indexed minter,
+        uint256 numberOfTokens
+    );
+
+    constructor() ERC721("Othmane 2 PyART collection","PART") Ownable(){
+        approval[msg.sender] = true ;
+        maxSupply = 50 ;
+    }
+
+    function uri(uint256 _tokenId) public view returns (string memory){
+        return string(abi.encodePacked(
+            "https://gateway.pinata.cloud/ipfs/QmQmLtPy29KZuGyrNMKvvWkCFdBqk3ZrWUU9bGXjS4KUZ3/",
+            Strings.toString(_tokenId),
+            ".json"
+        ));
+    }
+
+    function tokenURI(uint tokenID) override public view virtual returns(string memory){
+        return uri(tokenID);
+    }
+
+    function balanceOfContract() public view returns(uint256){
+        return address(this).balance ;
+    }
+
+    function retrieveEther(address payable recipient) external payable onlyOwner {
+        recipient.transfer(address(this).balance);
+    }
+
+    function getApproved(address user) public onlyOwner{
+        approval[user] = true ;
     }
 
     
 
-    function mint(uint amount) public{
-        require(totalsupply < Supply, "No NFTs left to mint.") ;
-        uint tokenId = totalsupply ;
-        for (uint i = 1; i <= amount; i ++){
-            _mint(msg.sender, (tokenId + i)) ;
+    function mint(uint256 amount) public payable{
+        require(msg.value >= amount * price, "inputted wong value");
+        require(totalsupply + amount < maxSupply, "exceeded NFT supply");
+        uint tokenID ;
+        for (uint i = 0; i < amount; i ++){
+            tokenID = totalsupply + i ;
+            _mint(msg.sender, (totalsupply + i));
+            NFTstock[totalsupply + i] = false ;
             totalsupply += 1 ;
-            NFTstock[tokenId] = false ;
-            NFTOwner[tokenId] = msg.sender ;
-        }
+            emit NFTminted(tokenID, block.timestamp, msg.sender, amount) ;
+        }    
+            
+
     }
 
 }
--- smart-contracts/Staking721.sol
+++ smart-contracts/Staking721.sol
@@ -4,69 +4,92 @@ pragma solidity ^0.8.14 ;
 
 
 import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol" ;
-//import "@openzeppelin/contracts/access/Ownable.sol";
 import "Token20.sol";
 import "NFT721.sol" ;
 
+
+/* ***************************************************
+   * This contract is uncomplete -                   *
+   * first version already deployed at this address :*
+   * 0x57A0B2876391d34D9Afc804E64477211aB518640      *
+   ***************************************************   */
 
 
 contract Staking721 is IERC721Receiver{
     
     Token20 token ;
     NFT721 NFT ;
-    /*IERC721 public NFT ;
-    IERC20 public token ; */
 
     constructor(Token20 _token, NFT721 _NFT){
         token = _token ;
         NFT = _NFT ;
-        // make this contract admin of Token20.sol
-        // make this contract approved in NFT721.sol
-    }
+    /*  ************************************************
+        * make this contract admin of Token20.sol      *
+        * make this contract approved in NFT721.sol    *
+        ************************************************ */
+    }  
+
+
     struct Stake {
             uint256 id ;
-            uint256 timestamp ;
+            uint256 timestamp ; // which represents the time it was staked at
     }
 
+    event NFTStaked(
+        uint256 indexed tokenID,
+        address indexed owner,
+        uint256 indexed timestamp
+    );
+    
+    event NFTUnstaked(
+        uint256 indexed tokenID,
+        address indexed owner,
+        uint256 indexed timestamp
+    );
 
     mapping(address => Stake) public stakes ;
 
     // this mapping will take care of checking how much time the address has staked one of her tokens.
     mapping(address => mapping(uint256 => uint256)) public amountTimeStaked ;
     
-    mapping(uint256 => address) public ownerStakedNFTs ;
+    mapping(uint256 => address) public ownerNFTs ; // this helps for keeping track of the user who stacked an NFT (bcz now contract owns it)
 
     function stake(uint256 tokenId) public {
         require(NFT.ownerOf(tokenId) == msg.sender, "You are not the owner of this NFT") ;
         stakes[msg.sender] = Stake(tokenId, block.timestamp);
-        ownerStakedNFTs[tokenId] = msg.sender ;
+        ownerNFTs[tokenId] = msg.sender ;
         NFT.safeTransferFrom(msg.sender, address(this), tokenId, "0x00") ;
-       
+        emit NFTStaked(tokenId, msg.sender, block.timestamp);
     }
 
-    function unstake(uint256 tokenId) public {
-        require(stakes[msg.sender].id == tokenId, "Please input NFT id that you own");
-        token.getUserApproved(msg.sender, true) ;
-        // to be able to mint tokens we need to be approved.
-        NFT.safeTransferFrom(address(this), msg.sender, tokenId, "0x00");
-        // will give us the time the token was staked in seconds.
-        amountTimeStaked[msg.sender][tokenId] = (block.timestamp - stakes[msg.sender].timestamp) ;
-        getReward(msg.sender, amountTimeStaked[msg.sender][tokenId]);
-        token.getUserApproved(msg.sender, false) ;
+
+    function unstake(uint256 tokenID) public {
+        require(ownerNFTs[tokenID]==msg.sender, "Please input NFT id that you own");
+        NFT.safeTransferFrom(address(this), msg.sender, tokenID, "0x00");
+        amountTimeStaked[msg.sender][tokenID] = (block.timestamp - stakes[msg.sender].timestamp) ;
         delete stakes[msg.sender] ;
+        emit NFTUnstaked(tokenID, msg.sender, block.timestamp);
     }
 
-    function getReward(address recipient, uint256 amountTime) private {
-        // reward rate : 3 tokens per 5 seconds 
-        uint256 time = amountTime ;
-        uint256 reward = 3*time ;
-        token.mint(recipient, reward);
+    function getReward(uint256 tokenId) external {
+        require(ownerNFTs[tokenId]==msg.sender, "not your NFT");
+        require(amountTimeStaked[msg.sender][tokenId] >=10, "NFT not staked enough time");
+        if (NFT.ownerOf(tokenId) == address(this)){
+            amountTimeStaked[msg.sender][tokenId] = (block.timestamp - stakes[msg.sender].timestamp) ;
+            uint256 time = amountTimeStaked[msg.sender][tokenId] ;
+            uint256 reward = time*3 ; // 3 tokens per seconds
+            token.mint(msg.sender, reward) ;
+            stakes[msg.sender].timestamp = block.timestamp ;
+        } 
+        else {
+            uint256 time = amountTimeStaked[msg.sender][tokenId] ;
+            uint256 reward = time*3 ; // 3 tokens per seconds
+            token.mint(msg.sender, reward) ;
+        }
+        
     }
 
-    function getOwnerOfStakedNFT(uint256 tokenId) public view returns(address){
-        require(ownerStakedNFTs[tokenId] != address(0), "This nft is not staked at the moment");
-        return ownerStakedNFTs[tokenId] ;
-    }
+   
 
     function onERC721Received(
         address,
@@ -75,5 +98,7 @@ contract Staking721 is IERC721Receiver{
         bytes calldata
     ) external pure override returns (bytes4) {
       return IERC721Receiver.onERC721Received.selector;
+
+    
     }
 }
